864ce54f0777c978ee85058718354fc1f3138660
Gian Carlo Pace <giancarlo.pace@hyland.com>
2023-04-04 14:44:25 +0200

    refactor: XDate is now a record.
    I still don't understand if the paratemeter of the record should be uppercase or not.

--- BirthdayGreetings/XDate.cs
+++ BirthdayGreetings/XDate.cs
@@ -1,36 +1,11 @@
 ﻿namespace BirthdayGreetings;
 
-public class XDate
+public record XDate(int Day, int Month)
 {
-  public XDate()
-  {
-    var today = DateTime.Today;
-    Day = today.Day;
-    Month = today.Month;
-  }
+  public XDate() : this(DateTime.Today.Day, DateTime.Today.Month) { }
 
-  public XDate(string yyyyMMdd)
-  {
-    Day = int.Parse(yyyyMMdd[8..]);
-    Month = int.Parse(yyyyMMdd.Substring(5, 2));
-  }
-
-  public int Day { get; }
-
-  public int Month { get; }
+  public XDate(string yyyyMMdd) : this(int.Parse(yyyyMMdd[8..]), int.Parse(yyyyMMdd.Substring(5, 2))) { }
 
   public bool IsSameDay(XDate anotherDate)
-    => anotherDate.Day == this.Day && anotherDate.Month == this.Month;
-
-  public override int GetHashCode() => Day;
-
-  public override bool Equals(Object? obj)
-  {
-    if (obj is not XDate other)
-      return false;
-
-    return this.Day == other.Day && this.Month == other.Month;
-  }
-
-  public override string ToString() => $"{Day}/{Month}";
+    => anotherDate.Day == Day && anotherDate.Month == Month;
 }
